0d4ff314acbc13bb5a9117a9fc9870c0582d30f8
Nelcione Dias <nelcionedias@gmail.com>
2022-10-06 12:56:04 -0300

    type string

---
+++ 03 - Tipos Primitivos/01 - string/index.js
@@ -0,0 +1,15 @@
+
+// console.log("Olá Mundo!");
+
+/**
+ * Escrita da string
+ 'Aqui temos uma string'
+ "Aqui temos uma String"
+ `Aqui temos uma string`
+ */
+ const name = 'Nelcione';
+
+ console.log("Olá Mundo!");
+ console.log('Olá Mundo!');
+ console.log(`Olá Mundo! ${ name }`);
+ 

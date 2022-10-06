134626bf6ca12b51833487d6484bad956e88e582
Nelcione Dias <nelcionedias@gmail.com>
2022-10-06 13:38:31 -0300

    type Number JS

---
+++ 03 - Tipos Primitivos/02 -  Number/index.js
@@ -0,0 +1,20 @@
+console.log(123);
+console.log(123 + 1);
+console.log('123');
+console.log('123' + 1);
+// console.log(string('123') - 1);
+console.log("*****************************")
+console.log(typeof Number('123'));
+console.log(typeof 123);
+console.log(typeof"123a");
+
+/**
+ //Forma correta de utilizar um number
+ * Number 123
+ * NUmber 123 + 123
+ * Number("123") + 123 = 246 
+ 
+ //Pode gerar erros
+ * Number "123" -1= 122
+ * Number "123" + 2 = 246
+ */

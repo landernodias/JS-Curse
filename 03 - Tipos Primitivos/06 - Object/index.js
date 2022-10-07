3ffa300d43287b85c8b47a7024d4af5db194b7ac
Nelcione Dias <nelcionedias@gmail.com>
2022-10-07 11:15:58 -0300

    exercicios

---
+++ 03 - Tipos Primitivos/06 - Object/index.js
@@ -0,0 +1,62 @@
+/**
+ * Objeto: Tenis (Propriedade) "cardaço", "palmilha"
+ * Objeto: Celular (Propriedade)
+ * Objeto: Drone (Propriedade)
+ * 
+ * 
+ * Propriedades / Atributos / Funcionalidades
+ * 
+ */
+
+console.log(
+    {
+        tenis: [
+            {
+            cardaco: {
+                cor: "azul",
+                tamanho: "G"
+            },
+            Palmilha: {
+                testura: "macia",
+                cor: "preto",
+                tamanho: 44
+            },
+            preco: 150.55,
+            disponivel: false,
+            estoque: 150,
+        },
+        {
+            cardaco: {
+                cor: "branca",
+                tamanho: "G"
+            },
+            Palmilha: {
+                testura: "macia",
+                cor: "preto",
+                tamanho: 35
+            },
+            preco: 89.55,
+            disponivel: true,
+            estoque: 100,
+        }]
+    }
+)
+
+console.log([
+    {
+        tenis: {
+            cardaco: {
+                cor: "branca",
+                tamanho: "G"
+            },
+            Palmilha: {
+                testura: "macia",
+                cor: "preto",
+                tamanho: 35
+            },
+            preco: 89.55,
+            disponivel: true,
+            estoque: 100,
+        }
+    }
+]);
